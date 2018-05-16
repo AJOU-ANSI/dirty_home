36cc1066daf691fcd11104d6e02ee0ad798174f6
Acka1357 <Acka1357@gmail.com>
2018-05-16 20:48:48 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_shake():
+    return render_template("info.html")
+
+@app.route('/apc')
+def home_apc():
+    return render_template("main.html")
+
+@app.route('/registration')
+def registration():
+	return render_template("registration.html");
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
